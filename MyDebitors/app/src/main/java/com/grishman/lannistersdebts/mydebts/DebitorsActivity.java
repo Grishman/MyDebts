356432356d67157d3e8c1b41bff0117cb40bec7e
Andrew Grishman <grishman4life@gmail.com>
2015-03-23 23:59:23 +0200

    Add Debitors Activity Do some refactor

--- MyDebitors/app/src/main/java/com/grishman/lannistersdebts/mydebts/DebitorsActivity.java
+++ MyDebitors/app/src/main/java/com/grishman/lannistersdebts/mydebts/DebitorsActivity.java
@@ -1,27 +1,26 @@
 package com.grishman.lannistersdebts.mydebts;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.os.Build;
 
-//Lol
 
-public class MainActivity extends ActionBarActivity {
+public class DebitorsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_debitors);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainFragment())
+                    .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
     }
@@ -30,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_debitors, menu);
         return true;
     }
 
@@ -52,25 +51,15 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class MainFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment {
 
-        private Button myDebitors;
-
-        public MainFragment() {
+        public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            myDebitors= (Button) rootView.findViewById(R.id.b_mydebitors);
-            myDebitors.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent startDebitorsActivity = new Intent(getActivity(), DebitorsActivity.class);
-                    startActivity(startDebitorsActivity);
-                }
-            });
+            View rootView = inflater.inflate(R.layout.fragment_debitors, container, false);
             return rootView;
         }
     }
